78178875a0e79de935058e5c31d346d32058eabf
Jo. H <44933755+p4jo@users.noreply.github.com>
2022-04-07 22:55:00 +0200

    include saveLog into Automate
    Also all code is english now (not the template though)

--- automate.ps1
+++ automate.ps1
@@ -17,9 +17,11 @@
     [Alias("log", "l")]
     [switch] $saveLog = $false,
 
-    [Alias("h", "help", "-help")]
+    [Alias("h", "help")]
     [switch] $getHelp = $false
 )
+
+Write-Host "This is JHAutomate." -ForegroundColor Green
 
 if ($getHelp) {
     Write-Host "This script auto generates a file with an \input statement for every .tex file in the folder specified with -p"
@@ -33,25 +35,27 @@ function getIncludedTexFiles{
         [string] $excludeFile = "",
         [switch] $log = $false
     )
-    $allProjectTexFiles = (Get-ChildItem -Path $path -Filter "*.tex" -Exclude $excludeFile, _*, *_ -Recurse)
-    $ProjectTexFiles = $allProjectTexFiles | Where-Object {-not (Test-Path (-join($_.Directory.FullName, "\.texignore")))} 
+    $allProjectFiles = (Get-ChildItem -Path $path -Filter "*.tex" -Exclude $excludeFile, _*, *_ -Recurse)
+    $includedProjectFiles = $allProjectFiles | Where-Object {-not (Test-Path (-join($_.Directory.FullName, "\.texignore")))} 
     
     if($log) {
-        $nonProjectFiles = $allProjectTexFiles | Where-Object {$ProjectTexFiles -notcontains $_}
-        $nonProjectFiles | ForEach-Object {
+        $excludedProjectFiles = $allProjectFiles | Where-Object {$includedProjectFiles -notcontains $_}
+
+        $excludedProjectFiles | ForEach-Object {
                 Write-Host "`tSkipped`t" -ForegroundColor Red -NoNewline
                 Write-Host "`"$_`"." -ForegroundColor Gray
             }
-        $ProjectTexFiles | ForEach-Object {
+
+        $includedProjectFiles | ForEach-Object {
                 Write-Host "`tAdded`t" -ForegroundColor Green -NoNewline
                 Write-Host "`"$_`"." -ForegroundColor Gray
             }
     }
 
-    return $ProjectTexFiles
+    return $includedProjectFiles
 }
 
-function inputGeneratedContent ($path, $rootPath, $excludeFile) {
+function generatedContent ($path, $rootPath, $excludeFile) {
     
     $currentPath = $path.FullName.Replace("\","/")
     $texstring = ""     
@@ -72,13 +76,16 @@ $texstring
 }
 
 function MakeNewRootFile($name, $contentFileName) {
-    $PfadHauptdatei = "./$name.tex"
+    $pathMainFile = "./$name.tex"
     Write-Host "Will write a new main file for $name. The old one will be backed up." -ForegroundColor Yellow
-    if(Test-Path "$PfadHauptdatei.bak"){
-        Remove-Item "$PfadHauptdatei.bak"
+
+    $mainFileExists = (Test-Path $pathMainFile)
+    $backupFileExists = (Test-Path $pathMainFile.bak)
+    if($mainFileExists -and $backupFileExists){
+        Remove-Item $pathMainFile.bak
     }
-    if(Test-Path $PfadHauptdatei){
-        Rename-Item $PfadHauptdatei "$PfadHauptdatei.bak"
+    if($mainFileExists){
+        Rename-Item $pathMainFile $pathMainFile.bak
     }
     
 "\newcommand\TITLE{$name}
@@ -93,20 +100,16 @@ function MakeNewRootFile($name, $contentFileName) {
 
 \RequirePackage{icomma} %macht Kommas ohne Leerzeichen danach zu Kommas ohne Abstand danach
 
-
-% \input{$name/_commands.tex}   
-
-
 \hypersetup{
-    % pdfsubject  = {$name},
-    % pdfkeywords = {$name}
+    pdfsubject  = {$name},
+    pdfkeywords = {$name}
 }
 
 \addbibresource{./$name/Referenzen.bib}
 
 \begin{document}
-%%% Titelseite hier (nur, wenn nicht simpleTitle als Option gesetzt wurde) :
-% \input{$name/_Titelseite.tex}
+
+%%% Titelseite hier (nur, wenn nicht simpleTitle als Option gesetzt wurde)
 
 \Inhaltsverzeichnis
 
@@ -114,11 +117,11 @@ function MakeNewRootFile($name, $contentFileName) {
 
 \input{$name/$contentFileName}
 
-\end{document}" | Out-File -FilePath $PfadHauptdatei -Encoding utf8 
+\end{document}" | Out-File -FilePath $pathMainFile -Encoding utf8 
            
 }
 
-# SKRIPT START
+# FIX INPUTS AND PATHS
 
 if ($BaseDirectory -ne ""){
     Set-Location $BaseDirectory
@@ -127,18 +130,21 @@ else{
     $BaseDirectory = Get-Location
 }
 
-Write-Host "This is JHAutomate." -ForegroundColor Green
 Write-Host "Base directory = ""$BaseDirectory"", Author for new main files: ""$author""."
 
+$project = $project.TrimEnd(".tex").TrimEnd('/').TrimEnd('\')
 
 $projectExists = (Test-Path $project)
+
+$onlyOneFolder = $false
+
 if ($project -eq "") {
-    $onlyOneFolder = $false
     Write-Host "Will run on all projects because you didn't specify a project using -project or -folder or -only."
 } else {
-    $project = $project.Substring($project.LastIndexOf('/')+1).Replace(".tex","")
+    $project = $project.Substring($project.LastIndexOf('/')+1)
     if ($projectExists) {
         Write-Host "Project (subdirectory) = ""$project"""
+        $onlyOneFolder = $true
     }
     else {
         if ($AllIfFolderDoesntExist) { 
@@ -146,39 +152,40 @@ if ($project -eq "") {
         }
         else {
             Write-Host "The specified project subdirectory $project doesn't exist. Specify -all (-AllIfFolderDoesntExist) or don't specify -project to regenerate all."
-        }        
-        $onlyOneFolder =  -not $AllIfFolderDoesntExist
+            $onlyOneFolder = $true
+        }
     }
 }
 
-# ANFANG 
+# BEGIN MAIN SCRIPT
 
-# foreach(Ordner $_ in hier)
-Get-ChildItem -Directory -Exclude .*, _* | 
-ForEach-Object { 
-    $name = $_.Name
-    $Dateiname = -join($name,"_generated.tex")
+# foreach(let $_: folder in $BaseDirectory)
+Get-ChildItem -Directory -Exclude .*, _* | ForEach-Object { 
+
+    $name = $_.Name # The name of a project is its folder's name.
+    $nameGeneratedFile = -join($name,"_generated.tex")
 
     if ($onlyOneFolder -and $name -ne $project) {
         Write-Host "Skipped $name"
     }
     else {
-        $PfadInnereAuflistungsTex = -join("./", $name, "/", $Dateiname)
+        $pathGeneratedFile = "./$name/$nameGeneratedFile"
 
         Write-Host "`nPlan to write the following references to the file " -NoNewline
-        Write-Host $PfadInnereAuflistungsTex -ForegroundColor Cyan
+        Write-Host $pathGeneratedFile -ForegroundColor Cyan
 
-        $fileContent = inputGeneratedContent -path $_ -excludeFile $Dateiname -rootPath "../$name.tex"
+        $fileContent = generatedContent -path $_ -excludeFile $nameGeneratedFile -rootPath "../$name.tex"
         if (
             ($fileContent -ne "") -and -not (
-                (Test-Path $PfadInnereAuflistungsTex) -and
-                ($fileContent -eq (Get-Content $PfadInnereAuflistungsTex -Encoding utf8 -Raw)[0])
+                (Test-Path $pathGeneratedFile) -and
+                ($fileContent -eq (Get-Content $pathGeneratedFile -Encoding utf8 -Raw)[0])
             )
         ){
-            $fileContent | Out-File $PfadInnereAuflistungsTex -Encoding utf8
+            $fileContent | Out-File $pathGeneratedFile -Encoding utf8
         }
     }
-    # Schreibe, wenn nicht vorhanden, eine Hauptdatei
+
+    # Generate main file from template for new projects, or overwrite for the specified project
     if ((
             $RenewMainFile -and
             $name -eq $project
@@ -186,6 +193,17 @@ ForEach-Object {
             -not (Test-Path "./$name.tex") -and
             (getIncludedTexFiles $_ ).Length -gt 0
         )) {
-        MakeNewRootFile -name $name -contentFileName $Dateiname
+        MakeNewRootFile -name $name -contentFileName $nameGeneratedFile
+    }
+}
+
+# Save Log file from being overwritten by quick rerender
+if ($saveLog) {
+
+    $logfile = "$project.log"
+    $destination = "$project\.log"
+
+    if ((Test-Path $logfile) -and (Test-Path $project)) {
+        Copy-Item $logfile -Destination $destination
     }
 }
