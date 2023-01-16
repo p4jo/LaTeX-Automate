0aa48499a861757bfdcae15aa2297c5764919eaa
Jo. H <44933755+p4jo@users.noreply.github.com>
2023-01-16 15:38:59 +0100

    document automate.ps1 and fix copying to OneDrive.

---
+++ copyToOneDrive.ps1
@@ -0,0 +1,13 @@
+param(
+    [Alias( "f", "folder", "only", "p")]
+    [string] $folder = ""
+)
+
+if ($f -ne ""){
+    $p = (Get-Variable 'Path_OneDrive' -ErrorAction 'Ignore'); 
+    if ($p -and (Test-Path $p)) { 
+        Copy-Item -Path "$folder/*.pdf" -Destination "$p/Dok/LaTeX-output"
+    }
+}
+
+# Set this by editing $profile.AllUsersAllHosts or similar
