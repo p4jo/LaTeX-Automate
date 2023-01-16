eb26a37f5ec35d3cc02faec7018e23a4aef61a48
Jo. H <44933755+p4jo@users.noreply.github.com>
2023-01-16 13:59:48 +0100

    remove unnecessary file

--- keyboardLayoutCopy.ps1
+++
@@ -1,9 +0,0 @@
-sudo cp /home/johannesh/repos/LaTeX-privat/Automate/JHKeyboard.xkb /usr/share/X11/xkb/symbols/LayoutJH
-# cp ./Automate/JHKeyboard ~/.config/xkb/symbols/
-# cp ./Automate/evdev.xml ~/.config/xkb/rules/
-# setxkbmap -layout JHKeyboard -variant JHGerman -v
-
-# just once: 
-# sudo ln -s ./Automate/JHKeyboard.xkb /usr/share/X11/xkb/symbols/JHKeyboard
-sudo ln -s /home/johannesh/repos/LaTeX-privat/Automate/.XCompose ~/.XCompose
-ibus restart
